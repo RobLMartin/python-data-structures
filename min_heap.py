42df8787c4723400d6a98f18c613f9249eac4d2a
Rob Martin <rmartin3@alumni.unca.edu>
2024-04-14 16:30:58 -0400

    insert

--- min_heap.py
+++ min_heap.py
@@ -2,7 +2,20 @@ class MinHeap:
   def __init__(self):
     self.heap = []
 
+  def insert(self, element):
+    self.heap.append(element)
+    self._heapify_up(len(self.heap) - 1)
+
+  def _heapify_up(self, index):
+    parent_index = (index - 1) // 2
+    if index > 0 and self.heap[index] <self.heap[parent_index]:
+      self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+      self._heapify_up(parent_index)
 
 if __name__ == "__main__":
   min_heap = MinHeap()
+  min_heap.insert(5)
+  min_heap.insert(3)
+  min_heap.insert(8)
+  min_heap.insert(1)
   print(min_heap.heap)
