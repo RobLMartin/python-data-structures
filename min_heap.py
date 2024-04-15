80c1be8db8fb08076af2e9b09f5119d0750eab21
Rob Martin <rmartin3@alumni.unca.edu>
2024-04-14 23:55:55 -0400

    min heap helpers

--- min_heap.py
+++ min_heap.py
@@ -1,52 +1,43 @@
 class MinHeap:
-  def __init__(self):
-    self.heap = []
+  def __init__(self, capacity):
+    self.storage = [0] * capacity
+    self.capacity = capacity
+    self.size = 0
 
-  def insert(self, element):
-    self.heap.append(element)
-    self._heapify_up(len(self.heap) - 1)
+  # Helpers
+  def get_parent_index(self, index):
+    return (index - 1) // 2
+  
+  def get_left_child_index(self, index):
+    return 2 * index + 1
+  
+  def get_right_child_index(self, index):
+    return 2 * index + 2
 
-  def _heapify_up(self, index):
-    parent_index = (index - 1) // 2
-    if index > 0 and self.heap[index] < self.heap[parent_index]:
-      self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
-      self._heapify_up(parent_index)
-
-  def pop_min(self):
-    if len(self.heap) == 0:
-      raise IndexError("pop from empty heap")
-
-    min_item = self.heap[0]
-    self.heap[0] = self.heap.pop()
-    self._heapify_down(0)
-    return min_item
-
-  def _heapify_down(self, index):
-    smallest = index
-    left_index = 2 * index + 1
-    right_index = 2 * index + 2
-
-    if left_index < len(self.heap) and self.heap[left_index] < self.heap[smallest]:
-      smallest = left_index
-    if right_index < len(self.heap) and self.heap[right_index] < self.heap[smallest]:
-      smallest = right_index
-
-    if smallest != index:
-      self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
-      self._heapify_down(smallest)
+  def has_parent(self, index):
+    return self.get_parent_index(index) >= 0
+  
+  def has_left_child(self, index):
+    return self.get_left_child_index < self.size
+  
+  def has_right_child(self, index):
+    return self.get_right_child_index(index) > self.size
+  
+  def parent(self, index):
+    return self.storage[self.get_parent_index(index)]
+  
+  def left_child(self, index):
+    return self.storage[self.get_left_child_index(index)]
+  
+  def right_child(self, index):
+    return self.storage[self.get_right_child_index(index)]
+  
+  def is_full(self):
+    return self.size == self.capacity
+  
+  def swap(self, index1, index2):
+    self.storage[index1], self.storage[index2] = self.storage[index2], self.storage[index1]
 
 
 if __name__ == "__main__":
   min_heap = MinHeap()
-  min_heap.insert(1)
-  min_heap.insert(3)
-  min_heap.insert(8)
-  min_heap.insert(5)
-  min_heap.insert(9)
-  min_heap.insert(14)
-  min_heap.insert(11)
-  min_heap.insert(10)
-  min_heap.insert(21)
-  print(min_heap.heap)
-  # print("Removed min:", min_heap.pop_min())
-  # print("New heap:", min_heap.heap)
